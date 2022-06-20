6e5b22c64f272340fe215ad31ce49d7e3953fde1
ikyxyzu <107379438+ikyxyzu@users.noreply.github.com>
2022-06-20 07:20:26 +0700

    Create _layout.svelte

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Navbar from "../components/Navbar.svelte";
+    import Footer from "../components/Footer.svelte";
+</script>
+
+<!-- routify:options preload="proximity" -->
+<Navbar />
+<main class="flex-shrink-0">
+    <slot />
+</main>
+<Footer />
